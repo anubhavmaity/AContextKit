69eb2bb9f2b2ab85456a39e359fc486e64a75666
Isaac Flath <Isaac.flath@gmail.com>
2025-03-27 20:45:45 -0400

    bug fix

--- contextkit/__init__.py
+++ contextkit/__init__.py
@@ -1,7 +1,7 @@
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 
 from . import read
-from .read import read_url, read_gist, read_gh_file, read_file, read_dir, read_pdf, read_yt_transcript, read_google_sheet, read_gdoc, read_arxiv
+from .read import read_url, read_gist, read_gh_file, read_file, read_dir, read_pdf, read_yt_transcript, read_google_sheet, read_gdoc, read_arxiv, read_gh_repo
 
 __all__ = ["read", "read_url", "read_gist", "read_gh_file", "read_file", "read_dir", "read_pdf", "read_yt_transcript", "read_google_sheet", "read_gdoc", "read_arxiv", "read_gh_repo"]
 
